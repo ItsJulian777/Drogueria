99cd8b117306f2f1c2d92e2510ca674701f97911
Julian <julian030445@gmail.com>
2021-12-15 20:06:31 -0500

    update

--- Modelo/Conexion/configServer.php
+++ Modelo/Conexion/configServer.php
@@ -1,6 +1,6 @@
 <?php
-define("USER", "epiz_30601122");
-define("SERVER", "sql307.epizy.com");
-define("BD", "epiz_30601122_store");
-define("PASS", "Wqay4xJm9Oj2u3d");
+define("USER", "root");
+define("SERVER", "localhost");
+define("BD", "store");
+define("PASS", "");
 ?>
